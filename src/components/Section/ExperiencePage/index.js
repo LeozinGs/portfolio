061aa5a574a8a1116ea58730795e3a334a619a02
Leonardo <lokinho_ddtank@hotmail.com>
2024-10-06 03:36:43 -0300

    Cria página de experiências.

--- src/components/Section/ExperiencePage/index.js
+++ src/components/Section/ExperiencePage/index.js
@@ -7,7 +7,22 @@ const ExperiencePage = () => {
             id={'experience'}
         >
             <div className="experience">
-
+                <h1 className="experience-title">Experiências</h1>
+                <p className="experience-text">
+                    No meu tempo de estudo, passei por alguns conteúdos dos quais eu tentei adiquirir alguma experiência criando um projeto básico somente para fixar e entender os conceitos aprendidos nos cursos, alguns dos conteúdos com os quais tive uma experiência maior são:
+                </p>
+                <ul className="experience-list">
+                    <li className="experience-list-item">FlexBox e Grid</li>
+                    <li className="experience-list-item">JavaScript</li>
+                    <li className="experience-list-item">HTML</li>
+                    <li className="experience-list-item">Local Storage</li>
+                    <li className="experience-list-item">Criação de componentes em React</li>
+                    <li className="experience-list-item">Recuperação de API's</li>
+                    <li className="experience-list-item">Hooks React</li>
+                </ul>
+                <p className="experience-text">
+                    Entre outras coisas, pretendo expandir essa lista mas, por agora, estou focando em aprimorar ainda mais o conhecimento nessas áreas.
+                </p>
             </div>
         </Section>
     );
